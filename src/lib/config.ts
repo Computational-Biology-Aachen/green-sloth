46f58572e504377eb4a8bbe382eb2d3718f3a1d9
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-06-05 10:25:03 +0200

    fix: unify config

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+import { dev } from "$app/environment";
+
+export const title = "GreenSloth";
+export const description = "Green Sloth";
+export const url = dev
+  ? "http://localhost:5173/"
+  : "https://computational-biology-aachen.github.io/green-sloth/";
