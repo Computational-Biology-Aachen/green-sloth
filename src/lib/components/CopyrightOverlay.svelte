7a3f48f9b4aa031bbcacf92769734bcf214171af
Elouen_Corvest <elouen.corvest@rwth-aachen.de>
2026-07-16 17:08:27 +0200

    dev: new: added journal and copyright to meta and appropriate areas

---
+++ src/lib/components/CopyrightOverlay.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    let { license, journal }: { license: string; journal: string } = $props();
+</script>
+
+<div class="copyright-bug">
+  <span class="copyright-text">
+    © {license} - {journal}
+  </span>
+</div>
+
+<style>
+  .copyright-bug {
+    position: absolute;
+    bottom: 4%;
+
+    background-color: rgba(0, 0, 0, 0.7);
+    color: white;
+
+    border-top-right-radius: 12px;
+    padding: 8px 16px;
+
+    white-space: nowrap;
+
+    width: auto;
+    max-width: 85%;
+
+    pointer-events: none;
+  }
+</style>
